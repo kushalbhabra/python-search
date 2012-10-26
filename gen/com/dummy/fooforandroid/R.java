d7b82afd2a5387c7ffd3f6207274190f38f9696e
Kushal Bhabra <kushalbhabra@gmail.com>
2012-10-26 21:57:28 +0530

    aded close option

--- gen/com/dummy/fooforandroid/R.java
+++ gen/com/dummy/fooforandroid/R.java
@@ -16,12 +16,13 @@ public final class R {
         public static final int stat_sys_warning=0x7f020002;
     }
     public static final class id {
+        public static final int btn_crawl=0x7f060005;
+        public static final int btn_exit=0x7f060002;
         public static final int color_layout=0x7f060000;
-        public static final int listView1=0x7f060005;
+        public static final int listView1=0x7f060006;
         public static final int progress_bar=0x7f060001;
-        public static final int tv_crawl=0x7f060004;
-        public static final int tv_keyword=0x7f060003;
-        public static final int tv_path=0x7f060002;
+        public static final int tv_keyword=0x7f060004;
+        public static final int tv_path=0x7f060003;
     }
     public static final class layout {
         public static final int dialog=0x7f030000;
--- res/raw/script.py
+++ res/raw/script.py
@@ -7,31 +7,43 @@ import time
 droid=android.Android()
 
 def eventloop():
-  while True:
-    event=droid.eventWait().result
-    print event
-    if event["name"]=="click":
-      id=event["data"]["id"]
-      if id=="btn_crawl":
-		path=droid.fullQueryDetail("tv_path").result['text']      #path from user
-		search=droid.fullQueryDetail("tv_keyword").result['text']   #keyword from user
-		results=[]
-		
+	try:
+	    while True:
+			event=droid.eventWait().result
+			print event
+			if event["name"]=="click":
+				id=event["data"]["id"]
+				if id=="btn_exit":
+					droid.fullDismiss()
+					return
+				if id=="btn_crawl":
+					droid.fullSetProperty("btn_crawl","text","Crawling...")
+					path=droid.fullQueryDetail("tv_path").result['text']      #path from user
+					search=droid.fullQueryDetail("tv_keyword").result['text']   #keyword from user
+					results=[]
 
-		for j in os.walk(path):
-			for k in j[2]:
-				try:            
-					f = open(j[0]+'/'+k,"rb")
-					if search in f.read() :
-						results.append(j[0]+'/'+k)
-						droid.fullSetList("listView1",results)
-					f.close()
-				except Exception:
-					pass
-					
-    elif event["name"]=="screen":
-      if event["data"]=="destroy":
-        pass
+					for j in os.walk(path):
+						for k in j[2]:
+							try:            
+								f = open(j[0]+'/'+k,"rb")
+								if search in f.read() :
+									results.append(j[0]+k)
+									droid.fullSetList("listView1",results)
+								f.close()
+							except Exception:
+								droid.fullSetProperty("btn_crawl","text","Done.")
+								time.sleep(2)
+								droid.fullSetProperty("btn_crawl","text","Crawl!")
+								eventloop()
+					droid.fullSetProperty("btn_crawl","text","Done.")
+					time.sleep(2)
+					droid.fullSetProperty("btn_crawl","text","Crawl!")
+			elif event["name"]=="screen":
+			  if event["data"]=="destroy":
+			  	droid.fullDismiss()
+				return
+	except Exception:
+		droid.makeToast(Exception)
 
 print "Started"
 layout="""<?xml version="1.0" encoding="utf-8"?>
@@ -40,6 +52,13 @@ layout="""<?xml version="1.0" encoding="utf-8"?>
     android:layout_height="fill_parent"
     android:orientation="vertical" 
 	android:background="#aa000000">
+
+    <Button
+        android:id="@+id/btn_exit"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:background="@android:drawable/ic_menu_close_clear_cancel" 
+        android:layout_gravity="right"/>
 
     <EditText
         android:id="@+id/tv_path"
@@ -57,12 +76,17 @@ layout="""<?xml version="1.0" encoding="utf-8"?>
         android:hint="keyword"/>
 
 
-    <Button
-        android:id="@+id/btn_crawl"
-        android:layout_width="fill_parent"
-        android:layout_height="wrap_content"
-        android:text="CRAWL BABY CRAWL!"
-         />
+			<Button
+	        android:id="@+id/btn_crawl"
+	        android:layout_width="fill_parent"
+	        android:layout_height="wrap_content"
+	        android:text="Crawl!"
+	        android:textColor="#ffffffff"
+	        android:background="#ff880077"
+	         />
+
+    
+
 
     <ListView
         android:id="@+id/listView1"
